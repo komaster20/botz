799010a81032d4faa98bedaa01f3e17cded947e2
komaster20 <komaster200@gmail.com>
2023-12-19 14:00:59 -0800

    Add files via upload

---
+++ styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    colors: {
+        blue: '#136DA0', // Light gray
+        orange: 'white', // Light blue
+        text: {
+            main: 'white',
+        }
+    },
+    small_width: "760px",
+    xs_width: "390px",
+    md_width: "1050px",
+}
